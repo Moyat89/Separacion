c208793c8861de160ca60cd6b43743d1a133c0c4
Moyat89 <lagunamov@gmail.com>
2023-11-14 21:16:48 +0100

    parcel

--- dist/index.9f62bb01.js
+++
@@ -1,3 +0,0 @@
-//# sourceMappingURL=index.9f62bb01.js.map
-
-//# sourceMappingURL=index.9f62bb01.js.map
--- dist/index.ce004ab9.js
+++
@@ -1,3 +0,0 @@
-//# sourceMappingURL=index.ce004ab9.js.map
-
-//# sourceMappingURL=index.ce004ab9.js.map
