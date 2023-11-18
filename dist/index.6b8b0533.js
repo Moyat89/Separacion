3fa14d98171ae259911e53e54cacb84f812fb736
Moyat89 <lagunamov@gmail.com>
2023-11-19 00:16:50 +0100

    deleting dist directory

--- dist/index.6b8b0533.js
+++
@@ -1,3 +0,0 @@
-//# sourceMappingURL=index.6b8b0533.js.map
-
-//# sourceMappingURL=index.6b8b0533.js.map
--- dist/index.fd9ed0db.js
+++
@@ -1,3 +0,0 @@
-//# sourceMappingURL=index.fd9ed0db.js.map
-
-//# sourceMappingURL=index.fd9ed0db.js.map
