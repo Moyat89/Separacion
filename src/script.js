d11d2c8769c25dc7db9aa1bc12dbf46ab8032075
Moyat89 <lagunamov@gmail.com>
2023-11-19 09:13:19 +0100

    script manual

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 function checkin() {
   document.getElementById('nav').style.visibility = 'hidden';
   document.getElementById('text').style.visibility = 'hidden';
-  document.getElementById("desplegable").style.display = "flex";
-  document.getElementById("desplegable").style.justifyContent = "center";
-  document.getElementById("desplegable").style.backgroundColor = "initial";
+  document.getElementById("desplegable").style.display = 'flex';
+  document.getElementById("desplegable").style.justifyContent = 'center';
+  document.getElementById("desplegable").style.backgroundColor = 'initial';
   document.getElementById("desplegable").style.border = "none";
   document.getElementById("body").style.display = "flex";
   document.getElementById("body").style.justifyContent = "center";
